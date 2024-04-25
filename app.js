e1c7cc739c868de745af6dd947f41d2d8e29caef
Ibrahim Molade <ibrahimakande94@ayahoo.com>
2024-04-25 15:06:25 +0100

    Implement a basic GET movies api with pagination functionality based on query parameters

---
+++ app.js
@@ -0,0 +1,3 @@
+const numbersArray = [1, 2, 3, 4, 5];
+
+console.log(numbersArray.slice(1, 6));
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const { readFileSync } = require("fs");
 
+const movies = JSON.parse(readFileSync("data/movies.json", "utf-8"));
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+app.get("/v1/movies", (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    const filteredMovies = movies.slice(startIndex, endIndex);
+
+    res.status(200).json({
+      status: "Succesful",
+      count: filteredMovies.length,
+      data: filteredMovies,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "Error", message: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on Port: ${port}`);
